74a8a5ccd203660f17963800232e88e1ee13dd51
jahnavi <jahnavigogia02@gmail.com>
2023-06-23 01:25:47 +0530

    Implement Exit button

--- cli.py
+++ cli.py
@@ -7,9 +7,11 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(),key='todos',
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
+exit_button = sg.Button("Exit")
 window = sg.Window("My to-do App", layout=[[label],
                     [input_box, add_button],
-                    [list_box, edit_button]],
+                    [list_box, edit_button],
+                    [exit_button]],
                     font=("Helvetica", 20))
 while True:
     event, values = window.read()
@@ -29,6 +31,8 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Exit":
+            exit()
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
